afd3d439609fa2a835e1454238c7f39d86a52e66
Petar Kovač <Petar.Kovac@fer.hr>
2021-05-02 18:19:12 +0200

    Add basic ML model skeleton and config functionality

---
+++ demos/svm_demo.py
@@ -0,0 +1,61 @@
+import argparse
+import importlib
+from pathlib import Path
+
+from sklearn.metrics import accuracy_score
+
+from benchmark_system.example import parse_dataset, featurize
+from models.basic_model import BasicModel
+from pipeline.preprocessing import preprocess_and_tokenize, tf_idf_vectorization
+from util import dataloader
+
+# Test SVM with basic model.
+if __name__ == '__main__':
+    # Parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', type=str, help="Config file from which model and dataset is read")
+    args = parser.parse_args()
+    conf_path = Path(args.config)
+    # This works too
+    # conf_path = Path("..\configs\svm.py")
+
+    print("Using Podium:")
+    # Get configs
+    spec = importlib.util.spec_from_file_location('module', conf_path)
+    conf = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(conf)
+
+    # Get data
+    train_data = dataloader.load_train_data(conf.test_task, emojis=conf.test_emojis,
+                                            irony_hashtags=conf.test_irony_hashtags)
+    train_dataset = preprocess_and_tokenize(train_data, remove_punct=conf.remove_punctuation)
+    x, y = train_dataset.batch(add_padding=True)
+    tfidf_batch = tf_idf_vectorization(train_dataset, x)
+
+    # Train model via tf-idf
+    model = BasicModel(conf.backbone)
+    model.fit(tfidf_batch, y.ravel())
+    y_hat = model.predict(tfidf_batch)
+    acc = accuracy_score(y_hat, y.ravel())
+    print(f"Accuracy on the train set with tf-idf BOW: {acc:.4f}")
+
+    # Train model via dictionary indices
+    model = BasicModel(conf.backbone)
+    model.fit(x, y.ravel())
+    y_hat = model.predict(x)
+    acc = accuracy_score(y_hat, y.ravel())
+    print(f"Accuracy on the train set with dictionary indices: {acc:.4f}")
+
+    print("\nUsing benchmark:")
+    dataset_path = '../datasets/train/SemEval2018-T3-train-task' + conf.test_task
+    dataset_path += '_emoji' if conf.test_emojis else ''
+    dataset_path += '_ironyHashtags' if conf.test_irony_hashtags else ''
+    dataset_path += '.txt'
+
+    corpus, y =  parse_dataset(dataset_path)
+    x = featurize(corpus)
+
+    model.fit(x, y)
+    y_hat = model.predict(x)
+    acc = accuracy_score(y_hat, y)
+    print(f"Accuracy on the train set with benchmark system (tf-idf BOW): {acc:.4f}")
--- models/basic_model.py
+++ models/basic_model.py
@@ -1,13 +1,9 @@
-import argparse
-import importlib
-from pathlib import Path
-
+from sklearn.datasets import make_classification
 from sklearn.metrics import accuracy_score
-import numpy as np
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import LinearSVC
 
 from models.model import Model
-from pipeline.preprocessing import preprocess_and_tokenize, tf_idf_vectorization
-from util import dataloader
 
 
 class BasicModel(Model):
@@ -25,47 +21,11 @@ class BasicModel(Model):
         return self.backbone.predict(X, **params)
 
 
-"""
-# Indices to one-hot encoding over the entire dataset - does not work.
-def to_one_hot(x, vector_len):
-    output = np.zeros((x.shape[0], x.shape[1], vector_len), dtype=int)
-
-    for row in range(x.shape[0]):
-        output[row] = np.eye(vector_len)[x[row]]
-
-    return output
-"""
-
-# Test SVM with basic model.
+# Simple test usage
 if __name__ == '__main__':
-    # Parse args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('config', type=str, help="Config file from which model and dataset is read")
-    args = parser.parse_args()
-    conf_path = Path(args.config)
-
-    # Get configs
-    spec = importlib.util.spec_from_file_location("module", conf_path)
-    conf = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(conf)
-
-    # Get data
-    train_data = dataloader.load_train_data(conf.test_task, emojis=conf.test_emojis,
-                                            irony_hashtags=conf.test_irony_hashtags)
-    train_dataset = preprocess_and_tokenize(train_data, remove_punct=conf.remove_punctuation)
-    x, y = train_dataset.batch(add_padding=True)
-    tfidf_batch = tf_idf_vectorization(train_dataset, x)
-
-    # Train model via tf-idf
-    model = BasicModel(conf.backbone)
-    model.fit(tfidf_batch, y.ravel())
-    y_hat = model.predict(tfidf_batch)
-    acc = accuracy_score(y_hat, y.ravel())
-    print(f"Accuracy on the train set: {acc:.4f}")
-
-    # Train model via dictionary indices (instead of one-hot encoding :( )
-    model = BasicModel(conf.backbone)
-    model.fit(x, y.ravel())
-    y_hat = model.predict(x)
-    acc = accuracy_score(y_hat, y.ravel())
+    X, y = make_classification(n_features=4, random_state=11)
+    model = BasicModel(LinearSVC())
+    model.fit(X, y)
+    y_hat = model.predict(X)
+    acc = accuracy_score(y_hat, y)
     print(f"Accuracy on the train set: {acc:.4f}")
