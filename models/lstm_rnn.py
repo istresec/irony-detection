Fix LSTM-RNN model. Fix length calculation.

models/lstm_rnn.py
@@ -1,20 +1,22 @@
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from torch.nn.utils.rnn import pack_padded_sequence
 
-import torch.nn.functional as F
 import torch.nn as nn
 import torch
 
 
 class RNNClassifier(nn.Module):
-    def __init__(self, embedding, embed_dim=300, hidden_dim=300, num_labels=2):
+    def __init__(self, embedding, embed_dim=300, hidden_dim=300, num_labels=2, num_layers=2, dropout=0.2):
         super(RNNClassifier, self).__init__()
         self.embedding = embedding
+        self.n_layers = num_layers
+        self.hidden_dim = hidden_dim
         self.encoder = nn.LSTM(
             input_size=embed_dim,
             hidden_size=hidden_dim,
-            num_layers=5,
+            num_layers=num_layers,
             bidirectional=True,
-            dropout=0.2
+            dropout=dropout,
+            batch_first=True
         )
         self.decoder = nn.Sequential(
             nn.Linear(2*hidden_dim, hidden_dim),
@@ -22,16 +24,15 @@ class RNNClassifier(nn.Module):
             nn.Linear(hidden_dim, num_labels)
         )
 
+    def init_hidden(self, batch_size, device):
+        weight = next(self.parameters()).data
+        hidden = (weight.new(self.n_layers*2, batch_size, self.hidden_dim).zero_().to(device),
+                  weight.new(self.n_layers*2, batch_size, self.hidden_dim).zero_().to(device))
+        return hidden
+
     def forward(self, x, lengths):
         e = self.embedding(x)
-        h_pack = pack_padded_sequence(e,
-                                      lengths,
-                                      enforce_sorted=False,
-                                      batch_first=True)
-
-        _, h = self.encoder(h_pack) # [2L x B x H]
-        # Concat last state of left and right directions
-
-        h = torch.cat([h[-1], h[-2]], dim=-1) # [B x 2H]
-
-        return self.decoder(h)
+        packed_e = pack_padded_sequence(e, lengths, batch_first=True, enforce_sorted=False)
+        outputs, (hidden, cell) = self.encoder(packed_e)
+        hidden = torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)
+        return self.decoder(hidden)

util/utils.py
@@ -94,10 +94,8 @@ def train(model, data, data_valid, optimizer, criterion, device, path, num_label
         for batch_idx, (x, y) in tqdm.tqdm(enumerate(data), total=len(data)):
             model.zero_grad()
             x, y = x.to(device), y.squeeze().to(device)
-            lens = []
-            for i in range(x.shape[0]):
-                lens.append(x[i].shape[0])
-            logits = model(x, sorted(lens))
+            lens = get_lengths(x)
+            logits = model(x, lens)
             accuracy_t, conf_mat_t = update_stats(accuracy_t, conf_mat_t, logits, y)
             loss = criterion(logits, y)
             loss_t += loss.item()
@@ -134,13 +132,25 @@ def evaluate(model, data, device, criterion, num_labels=2):
     with torch.no_grad():
         for batch_idx, (x, y) in tqdm.tqdm(enumerate(data), total=len(data)):
             x, y = x.to(device), y.squeeze().to(device)
-            lens = []
-            for i in range(len(x)):
-                lens.append(len(x[i]))
-            logits = model(x, sorted(lens))
+            lens = get_lengths(x)
+            logits = model(x, lens)
             loss += criterion(logits, y).item()
             accuracy, confusion_matrix = update_stats(accuracy, confusion_matrix, logits, y)
     return loss, accuracy, confusion_matrix
+
+
+def get_lengths(x):
+    lens = []
+    for i in range(x.shape[0]):
+        idx_of_padding = (x[i] == 1).nonzero(as_tuple=True)[0]
+        if len(idx_of_padding) > 0:
+            idx_of_padding = idx_of_padding[0].detach().cpu().data.numpy().item()
+        else:
+            idx_of_padding = x.shape[1]
+        if idx_of_padding <= 0:
+            idx_of_padding = 1
+        lens.append(idx_of_padding)
+    return lens
 
 
 # test