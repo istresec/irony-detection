Add correct averaging in basic NN model, fix padding problem

models/basic_nn_model.py
@@ -15,7 +15,10 @@ class SimpleClassifier(nn.Module):
 
     def forward(self, x, lengths, features=None):
         e = self.embedding(x)
-        e = torch.mean(e, dim=1)
+
+        # Get embedding average using lengths
+        e = torch.sum(e, dim=1)
+        e = torch.div(e, torch.tensor(lengths).to(device=e.device)[:, None])
 
         e_and_f = torch.cat((e, features), dim=1) if self.features else e
 

util/utils.py
@@ -19,11 +19,11 @@ def load_and_preprocess(config, padding=False):
     :param padding: Determines if data is padded or not, False by default. Boolean.
     :return: The training data and labels x and y, the test data and labels x_test and y_test, and the vocab.
     """
-    # train_data, valid_data = load_train_data(config.test_task, emojis=config.test_emojis,
-    #                                          irony_hashtags=config.test_irony_hashtags, split=True)
-    # test_data = load_test_data(config.test_task, emojis=config.test_emojis)
+    train_data, valid_data = load_train_data(config.test_task, emojis=config.test_emojis,
+                                             irony_hashtags=config.test_irony_hashtags, split=True)
+    test_data = load_test_data(config.test_task, emojis=config.test_emojis)
 
-    train_data, valid_data, test_data = load_imdb()
+    # train_data, valid_data, test_data = load_imdb()
 
     train_dataset, vocab = preprocess_and_tokenize(train_data, remove_punct=config.remove_punctuation)
     test_dataset = preprocess_and_tokenize(test_data, remove_punct=config.remove_punctuation, use_vocab=False)
@@ -48,7 +48,7 @@ def load_and_preprocess(config, padding=False):
         for i in range(len(x)):
             tweet_len = len(x[i])
             if required_length > tweet_len:
-                x[i] = np.concatenate((x[i], vocab.numericalize(np.array(padding * (required_length - tweet_len)))))
+                x[i] = np.concatenate((x[i], np.array(vocab.numericalize(padding * (required_length - tweet_len)))))
         for tweet in x_v:
             tweet_len = len(tweet)
             if required_length > tweet_len: